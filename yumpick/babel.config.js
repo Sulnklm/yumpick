9b97f939b1fd9c49c8a573df2a695689a01b51e5
Suin Kim <hyeinsuin@icloud.com>
2024-12-15 13:50:39 -0800

    Delete yumpick directory

--- yumpick/babel.config.js
+++
@@ -1,7 +0,0 @@
-// babel.config.js
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],  // Expo 기본 프리셋 사용
-  };
-};
--- yumpick/screens/ProfileScreen.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, View, ImageBackground, } from 'react-native';
-
-export default function ProfileScreen ({ navigation }) {
-    return (
-        <View>
-            
-        </View>
-    )
-}
