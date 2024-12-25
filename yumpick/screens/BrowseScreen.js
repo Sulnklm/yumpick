1a5875684513c3f3e8c95c922509db7bf9039f27
rrc_f2023_Dongok Yang <dyang2@academic.rrc.ca>
2024-12-25 13:46:55 -0600

    Chore(restaurants) : added sample data inside restaurants file

--- yumpick/screens/BrowseScreen.js
+++ yumpick/screens/BrowseScreen.js
@@ -1,9 +1,5 @@
 import React from "react";
 
-export default function BrowseScreen() { 
-    return (
-        <>
-        </>
-    );
+export default function BrowseScreen() {
+  return <></>;
 }
-
